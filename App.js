c12f33de6abe7a5ef5f19cc2062ac55db2570819
Cory Booth <Rodimus2005@hotmail.com>
2018-07-18 13:39:33 -0500

    intial commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Picker, Button, SectionList, Image } from 'react-native';
+import FadeInView from './FadeInView';
+
+export default class App extends React.Component {
+
+
+
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        {/* <Text> Hello Tacos!!!!  More tacos!!!!</Text> */}
+        {/* <Picker
+          style={{ height: 50, width: 200 }}>
+          <Picker.Item label="Tacos" value="java" />
+          <Picker.Item label="More Tacos" value="js" />
+          <Picker.Item label="Even More Tacos" value="js" />
+        </Picker> */}
+        <Button
+          // onPress={}
+          title="Push For Tacos"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <FadeInView style={{ width: 250, height: 55, backgroundColor: 'powderblue' }}>
+          <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>Magic Tacos</Text>
+        </FadeInView>
+        <Image source={require('./images/HurricanePortable.jpg')} style={{ height: 500 }} />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
